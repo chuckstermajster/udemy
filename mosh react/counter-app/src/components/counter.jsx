be9dbbee4f3f1766aea6a810edc77ec360065482
Mateusz <chuckstermajster@gmail.com>
2018-12-10 21:57:45 +0100

    set up vidly project

--- mosh react/counter-app/src/components/counter.jsx
+++ mosh react/counter-app/src/components/counter.jsx
@@ -2,20 +2,42 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 5,
+    tags: ["tag1", "tag2", "tag3"]
   };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Incerement</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Incerement
+        </button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
 
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "primary" : "warning";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
