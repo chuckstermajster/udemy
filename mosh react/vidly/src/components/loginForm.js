35af9ab207b4a62e7bce599474e4a6db51df90ef
Mateusz <chuckstermajster@gmail.com>
2019-01-02 20:50:25 +0100

    implement validation using Joi

--- mosh react/vidly/src/components/loginForm.js
+++ mosh react/vidly/src/components/loginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Input from "./common/input";
+import Joi from "joi-browser";
 
 class LoginForm extends Component {
   state = {
@@ -7,24 +8,34 @@ class LoginForm extends Component {
     errors: {}
   };
 
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
   validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+
+    if (!error) return null;
+
     const errors = {};
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "username is required";
-    if (account.password.trim() === "")
-      errors.password = "password is required";
-    return Object.keys(errors).length === 0 ? null : errors;
+
+    for (let item of error.details) {
+      errors[item.path[0]] = item.message;
+    }
+    return errors;
   };
 
   validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") return "Username is required";
-    }
-
-    if (name === "password") {
-      if (value.trim() === "") return "Password is required";
-    }
+    const property = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(property, schema);
+    return error ? error.details[0].message : null;
   };
   handleChange = ({ currentTarget: input }) => {
     const errorMessage = this.validateProperty(input);
@@ -65,7 +76,9 @@ class LoginForm extends Component {
             error={errors.password}
           />
 
-          <button className="btn btn-primary">Submit</button>
+          <button className="btn btn-primary" disabled={this.validate()}>
+            Submit
+          </button>
         </form>
       </div>
     );
